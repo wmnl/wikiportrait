b360992f5f45095b67d82cd3f9c44ba88670de4f
sjoerddebruin <sjoerddebruin@me.com>
2014-06-08 11:03:56 +0200

    Temporary fix for mobile menu

--- menu.php
+++ menu.php
@@ -5,14 +5,16 @@
     <li><a href="<?php echo $basispad; ?>/index.php"><i class="fa fa-home fa-lg"></i><span>Home</span></a></li>
     <li><a href="<?php echo $basispad; ?>/wizard.php"><i class="fa fa-cloud-upload fa-lg"></i><span>Insturen</span></a></li>
     <li><a href="<?php echo $basispad; ?>/upload.php"><i class="fa fa-upload fa-lg"></i><span>Uploaden</span></a></li>
+    <div class="float-right">
     <?php
         if(isset($_SESSION['user']))
             {
-                echo "<li class=\"float-right\"><a href=\"$basispad/images.php\"><i class=\"fa fa-folder-open fa-lg\"></i><span>Inzendingen</span></a></li>";
+                echo "<li><a href=\"$basispad/images.php\"><i class=\"fa fa-folder-open fa-lg\"></i><span>Inzendingen</span></a></li>";
 
                 if($_SESSION['isSysop'] == true)
-                    echo "\n\t<li class=\"float-right\"><a href=\"$basispad/admin\"><i class=\"fa fa-wrench fa-lg\"></i><span>Beheer</span></a></li>";
+                    echo "\n\t<li><a href=\"$basispad/admin\"><i class=\"fa fa-wrench fa-lg\"></i><span>Beheer</span></a></li>";
             }
     ?>  
-    <li class="float-right"><?php if (isset($_SESSION['user'])) echo "<a href=\"$basispad/logout.php\"><i class=\"fa fa-sign-out fa-lg\"></i><span>Uitloggen</span></a>"; else echo "<a href=\"$basispad/login.php\"><i class=\"fa fa-sign-in fa-lg\"></i><span>Inloggen</span></a>"; ?></li>
+    <li><?php if (isset($_SESSION['user'])) echo "<a href=\"$basispad/logout.php\"><i class=\"fa fa-sign-out fa-lg\"></i><span>Uitloggen</span></a>"; else echo "<a href=\"$basispad/login.php\"><i class=\"fa fa-sign-in fa-lg\"></i><span>Inloggen</span></a>"; ?></li>
+    </div>
 </ul>   
