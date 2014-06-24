73627fd921243a27475a1aa298070362fb4b13ff
Southparkfan <southparkfan223@hotmail.com>
2014-06-24 16:32:49 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+# Database settings
+$DBserver = ''; // Name of the databaseserver. Most times "localhost" or "127.0.0.1" works.
+$DBuser   = ''; // Name of the databaseuser, must have the appropriate permissions on the database.
+$DBname   = ''; // Name of the database.
