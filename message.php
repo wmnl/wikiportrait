a8aba6c8178db8c87aec0e6412f21538d4458051
sjoerddebruin <sjoerddebruin@me.com>
2014-07-02 19:38:48 +0200

    Convert the damn spaces to tabs

--- message.php
+++ message.php
@@ -1,43 +1,39 @@
 <?php 
-    include 'header.php';
-    checkLogin();
+	include 'header.php';
+	checkLogin();
 ?>
 <div id="content">
-    <h2>Kant-en-klaar berichtje versturen</h2>
+	<h2>Kant-en-klaar berichtje versturen</h2>
 
-    <?php
-    
-        if (!isset($_GET['image']) || !isset($_GET['message']))
-        {
-            echo "<div class=\"error\"><ul>";
-            echo "<li>Bericht of afbeelding niet gevonden!</li>";
-            echo "</ul></div>";
-        }
-        else
-        {
-    
-    $query = sprintf("SELECT * FROM messages
-              LEFT JOIN users
-              ON users.id = '%d'
-              LEFT JOIN images
-              ON images.id = '%d'
-              WHERE messages.id = '%d'", mysql_real_escape_string($_SESSION['user']), mysql_real_escape_string($_GET['image']), mysql_real_escape_string($_GET['message']));
-    $result = mysql_query($query);
-    $row = mysql_fetch_assoc($result);
-    
-    $templates = array("%%name%%", "%%title%%", "%%otrsname%%");
-    $replace = array($row['name'], $row['title'], $row['otrsname']);
-    ?>
-    <textarea>
-        <?php 
-            echo str_replace($templates, $replace, $row['message']); 
-        ?>
-    </textarea>
-    <?php
-        }
-    ?>
-    <div class="clear"></div>
+	<?php
+	
+		if (!isset($_GET['image']) || !isset($_GET['message']))
+		{
+			echo "<div class=\"error\"><ul>";
+			echo "<li>Bericht of afbeelding niet gevonden!</li>";
+			echo "</ul></div>";
+		}
+		else
+		{
+	
+	$query = sprintf("SELECT * FROM messages
+			  LEFT JOIN users
+			  ON users.id = '%d'
+			  LEFT JOIN images
+			  ON images.id = '%d'
+			  WHERE messages.id = '%d'", mysql_real_escape_string($_SESSION['user']), mysql_real_escape_string($_GET['image']), mysql_real_escape_string($_GET['message']));
+	$result = mysql_query($query);
+	$row = mysql_fetch_assoc($result);
+	
+	$templates = array("%%name%%", "%%title%%", "%%otrsname%%");
+	$replace = array($row['name'], $row['title'], $row['otrsname']);
+	?>
+	<textarea><?php echo str_replace($templates, $replace, $row['message']); ?></textarea>
+	<?php
+		}
+	?>
+	<div class="clear"></div>
 </div>
 <?php
-    include 'footer.php';        
+	include 'footer.php';		 
 ?>
