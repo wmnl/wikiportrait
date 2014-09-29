95e3345c9a2a81836fbb52ff015c7987dbc2abc2
Jurgen <jurgen@hotmail.lv>
2014-09-29 11:33:45 +0200

    no message

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
     # Database settings
-    $DBserver = ''; // Name of the databaseserver. Most times "localhost" or "127.0.0.1" works.
-    $DBname   = ''; // Name of the database.
-    $DBuser   = ''; // Name of the databaseuser, must have the appropriate permissions on the database.
-    $DBpassword = ''; // Password of the user specified in $DBuser. Leave blank if no password is set (not recommended!).
+    $DBserver = 'server'; // Name of the databaseserver. Most times "localhost" or "127.0.0.1" works.
+    $DBname   = 'dbname'; // Name of the database.
+    $DBuser   = 'dbuser'; // Name of the databaseuser, must have the appropriate permissions on the database.
+    $DBpassword = 'dbpass'; // Password of the user specified in $DBuser. Leave blank if no password is set (not recommended!).
     # Mail settings
     $MailSTMPhost = ''; // Name of the STMP host. Most times "smtp.domain.com"
     $MailRecipient = ''; // TODO
