89d81e52e4c3013e7245501836bf8cae850e8e30
sjoerddebruin <sjoerddebruin@me.com>
2014-09-30 20:59:25 +0200

    Make some more buttons responsive

--- question.php
+++ question.php
@@ -12,19 +12,19 @@
         {
             echo "<h3>Het advies van de uploadwizard is:</h3>";
             echo "<div class=\"box red\">Niet uploaden</div>";
-            echo "<a class=\"button\" href=\"index.php\">Terug naar de startpagina</a>";
+            echo "<div class=\"bottom\"><a class=\"button\" href=\"index.php\">Terug naar de startpagina</a></div>";
         }
         elseif ($result == "success")
         {
             echo "<h3>Uploaden voltooid</h3>";
             echo "<div class=\"box green\">De afbeelding is met succes geüpload.<br />Een vrijwilliger zal de afbeelding zo snel mogelijk beoordelen en contact met u opnemen.<br />Als u wilt, kunt u <a href='track.php?image=" . $_GET['id'] . "&key=" . sha1($_GET['id']) . "'>hier</a> uw inzending volgen.</div>";
-            echo "<a class=\"button\" href=\"upload.php\">Nog een afbeelding uploaden</a>";
+            echo "<div class=\"bottom\"><a class=\"button\" href=\"upload.php\">Nog een afbeelding uploaden</a></div>";
         }
         else
         {
             echo "<h3>Het advies van de uploadwizard is:</h3>";
             echo "<div class=\"box green\">Upload de afbeelding onder de CC-BY-SA licentie</div>";
-            echo "<a class=\"button\" href=\"upload.php\"><i class=\"fa fa-cloud-upload fa-lg fa-fw\"></i>Uploaden</a>";
+            echo "<div class=\"bottom\"><a class=\"button\" href=\"upload.php\"><i class=\"fa fa-cloud-upload fa-lg fa-fw\"></i>Uploaden</a></div>";
         }
     }
 ?>
