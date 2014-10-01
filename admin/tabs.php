474a8934f2c8d6588c8472cefe6ce97e8d5bde08
Sjoerd de Bruin <sjoerddebruin@me.com>
2014-10-01 09:04:51 +0200

    Convert tabs to display:table;

--- admin/tabs.php
+++ admin/tabs.php
@@ -1,6 +1,7 @@
 <?php $page2 = pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_FILENAME); ?>
-<ul class="tabs">
-	<li<?php if ($page2 == 'users') echo ' class="active"'?>><a href="users.php"><i class="fa fa-users"></i>Gebruikers</a></li>
-	<li<?php if ($page2 == 'messages') echo ' class="active"'?>><a href="messages.php"><i class="fa fa-comments"></i>Berichten</a></li>
-	<div class="clear"></div>
-</ul>
+<div class="tabs">
+    <ul>
+        <li<?php if ($page2 == 'users') echo ' class="active"'?>><a href="users.php"><i class="fa fa-users"></i>Gebruikers</a></li>
+        <li<?php if ($page2 == 'messages') echo ' class="active"'?>><a href="messages.php"><i class="fa fa-comments"></i>Berichten</a></li>
+    </ul>
+</div>
