3c3627bdbe6e324a297a33ab47c4a6ca96ff0372
Southparkfan <Southparkfan@users.noreply.github.com>
2014-10-19 14:37:44 +0200

    Add $connection to mysqli_connect_error

--- config/connect.php
+++ config/connect.php
@@ -3,6 +3,6 @@
 	$connection = mysqli_connect($DBserver, $DBuser, $DBpassword, $DBname);
 	if (!$connection)
 	{
-	echo "An error occurred during connection: " . mysqli_connect_error();
+	echo "An error occurred during connection: " . mysqli_connect_error($connection);
 	}
 ?>
