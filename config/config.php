98e9944bc36ec363529120f2d6519e27ef2b1294
Southparkfan <Southparkfan@users.noreply.github.com>
2014-10-23 12:55:03 +0200

    Add debug to config.php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,13 @@
 <?php
 	# Database settings
-	$DBserver = ''; // Name of the databaseserver. Most times "localhost" or "127.0.0.1" works.
-	$DBname	  = ''; // Name of the database.
-	$DBuser	  = ''; // Name of the databaseuser, must have the appropriate permissions on the database.
-	$DBpassword = ''; // Password of the user specified in $DBuser. Leave blank if no password is set (not recommended!).
+	$DBserver 		= ''; // Name of the databaseserver. Most times "localhost" or "127.0.0.1" works.
+	$DBname	  		= ''; // Name of the database.
+	$DBuser	  		= ''; // Name of the databaseuser, must have the appropriate permissions on the database.
+	$DBpassword 		= ''; // Password of the user specified in $DBuser. Leave blank if no password is set (not recommended!).
 	# Mail settings
-	$MailSTMPhost = ''; // Name of the STMP host. Most times "smtp.domain.com"
-	$MailRecipient = ''; // TODO
-	$MailRecipientName = ''; // TODO
+	$MailSTMPhost 		= ''; // Name of the STMP host. Most times "smtp.domain.com"
+	$MailRecipient 		= ''; // TODO
+	$MailRecipientName 	= ''; // TODO
+	# Developer Settings
+	$DevDebugging 		= false; // Show PHP errors, by default false
 ?>
