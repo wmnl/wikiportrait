7477693ad022811d7791741dfda1884fa4ae484b
Sjoerd de Bruin <sjoerddebruin@me.com>
2014-11-03 09:19:48 +0100

    Add favicon.ico (#5)

--- header.php
+++ header.php
@@ -27,20 +27,24 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="initial-scale=1.0" />
-		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes" />
+		<meta name="application-name" content="Wikiportret" />
+		<!-- Apple -->
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="apple-mobile-web-app-title" content="Wikiportret">
-		<link rel="apple-touch-icon" href="<?php echo $basispad ?>/apple-touch-icon.png">
-		<link rel="icon" sizes="180x180" href="<?php echo $basispad ?>/apple-touch-icon.png">
+		<meta name="apple-mobile-web-app-title" content="Wikiportret" />
+		<link rel="apple-touch-icon" href="<?php echo $basispad ?>/icons/apple-touch-icon.png" />
+		<!-- Overige icons -->
+		<link rel="icon" type="image/png" href="<?php echo $basispad ?>/icons/apple-touch-icon.png" />
+		<link rel="shortcut icon" type="image/x-icon" href="<?php echo $basispad ?>/icons/favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="<?php echo $basispad ?>/style/style.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo $basispad ?>/style/responsive.css" />
 		<!--[if lt IE 8 ]>
 			<link rel="stylesheet" type="text/css" href="<?php echo $basispad ?>/style/ie.css" />
 		<![endif]-->
-	   	<link href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-	   	<link href="<?php echo $basispad ?>/scripts/jQuery.SimpleSelect/jquery.simpleselect.css" rel="stylesheet">
+	   	<link href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+	   	<link href="<?php echo $basispad ?>/scripts/jQuery.SimpleSelect/jquery.simpleselect.css" rel="stylesheet" />
 		<script src="<?php echo $basispad ?>/scripts/jquery-2.1.1.min.js"></script>
 		<title>Wikiportret - Stel uw foto's ter beschikking</title>
 	</head>
