a6865eda867605941715979a4cb19fee220ecdd5
Jurgen <jurgen@hotmail.lv>
2014-11-08 14:40:18 +0100

    Fix indent, clean up in phpmailer folder

--- images/search.php
+++ images/search.php
@@ -7,116 +7,113 @@ checkLogin();
 <div id="content">
 
 	<div class="page-header">
-		<h2>Zoeken</h2>
+	    <h2>Zoeken</h2>
 	</div>
-	        <form method="post">
+	    <form method="post">
 
-                <div class="input-container">
-                        <label for="search"><i class="fa fa-search"></i>Zoekterm</label>
-                        <input type="text" name="search" id="search" />
-                </div>
+	    <div class="input-container">
+		    <label for="search"><i class="fa fa-search"></i>Zoekterm</label>
+		    <input type="text" name="search" id="search" />
+	    </div>
 
-                <div class="bottom right">
-                        <button class="green" type="submit" name="postback"><i class="fa fa-search"></i>Zoeken</button>
-                </div>
+	    <div class="bottom right">
+		    <button class="green" type="submit" name="postback"><i class="fa fa-search"></i>Zoeken</button>
+	    </div>
 
         </form>
 	<br />
 
 	<?
 	if (!empty($errors))
+	{
+	    echo "<div class=\"box red\">";
+
+	    foreach ($errors as $error)
+	    {
+		echo "$error";
+	    }
+
+	    echo "</div>";
+	}
+	else
+	{
+	    if (isset($_POST['postback']))
+	    {
+		$errors = array();
+
+	    if (empty ($_POST['search']))
+	    {
+		array_push($errors, "Er is geen zoekterm ingevoerd");
+	    }
+
+	    if (empty($errors))
+	    {
+		$query = sprintf("SELECT *, users.otrsname AS owneruser FROM images INNER JOIN users ON images.owner = users.id WHERE title LIKE '%%%s%%'", mysqli_real_escape_string($connection, $_POST['search']));
+		$result = mysqli_query($connection, $query);
+
+		if (mysqli_num_rows($result) > 0)
 		{
-			echo "<div class=\"box red\">";
-
-			foreach ($errors as $error)
-			{
-				echo "$error";
-			}
-
-			echo "</div>";
-		}
-		else
-		{
-			if (isset($_POST['postback']))
-			{
-				$errors = array();
-
-			if (empty ($_POST['search']))
-			{
-				array_push($errors, "Er is geen zoekterm ingevoerd");
-			}
-
-			if (empty($errors))
-			{
-				$query = sprintf("SELECT *, users.otrsname AS owneruser FROM images INNER JOIN users ON images.owner = users.id WHERE title LIKE '%%%s%%'", mysqli_real_escape_string($connection, $_POST['search']));
-				$result = mysqli_query($connection, $query);
-
-				if (mysqli_num_rows($result) > 0)
-				{
-				?>
+	?>
 
 	<div class="table-container">
 
-		<table>
-				<thead>
-					<tr>
-						<th>Foto</th>
-						<th>Titel</th>
-						<th>Uploader</th>
-						<th>Datum</th>
-						<th>Eigenaar</th>
-						<th class="actions-1">Acties</th>
-					</tr>
-				</thead>
+	    <table>
+		<thead>
+		    <tr>
+			<th>Foto</th>
+			<th>Titel</th>
+			<th>Uploader</th>
+			<th>Datum</th>
+			<th>Eigenaar</th>
+			<th class="actions-1">Acties</th>
+		    </tr>
+		</thead>
 
-				<tbody>
-				<?php
-					while ($row = mysqli_fetch_assoc($result)):
-						$id = $row['id'];
-						$filename = htmlspecialchars($row['filename']);
-						$title = htmlspecialchars($row['title']);
-						$name = htmlspecialchars($row['name']);
-						$owner = htmlspecialchars($row['owneruser']);
-						$timestamp = $row['timestamp'];
-				?>
-					<tr>
-
-						<td data-title="Foto" class="image"><a href="single.php?id=<?php echo $id ?>"><img src="../uploads/<?php echo $filename?>" /></a></td>
-						<td data-title="Titel"><a href="single.php?id=<?php echo $id ?>"><?php echo $title ?></a></td>
-						<td data-title="Uploader"><?php echo $name ?></td>
-						<td data-title="Datum"><?php echo strftime("%e %B %Y", $timestamp) ?></td>
-						<td data-title="Eigenaar"><?= $owner ?></td>
-						<td data-title="Acties" class="center"><a class="button" href="single.php?id=<?php echo $id ?>"><i class="fa fa-info"></i>Details</a></td>
-					</tr>
-					<?php
-					 endwhile;
-					?>
-				</tbody>
-		</table>
-		
+		<tbody>
 		<?php
-
-				}
-				else
-				{
-					echo "<div class=\"box red\">Er zijn geen resultaten gevonden.</div>";
-				}
-			}
-			else
-			{
-			echo "<div class=\"box red\"><ul>";
-
-			foreach ($errors as $error)
-			{
-				echo "<li>$error</li>";
-			}
-
-			echo "</ul></div>";
-			}
+		    while ($row = mysqli_fetch_assoc($result)):
+			$id = $row['id'];
+			$filename = htmlspecialchars($row['filename']);
+			$title = htmlspecialchars($row['title']);
+			$name = htmlspecialchars($row['name']);
+			$owner = htmlspecialchars($row['owneruser']);
+			$timestamp = $row['timestamp'];
+		?>
+			<tr>
+			    <td data-title="Foto" class="image"><a href="single.php?id=<?php echo $id ?>"><img src="../uploads/<?php echo $filename?>" /></a></td>
+			    <td data-title="Titel"><a href="single.php?id=<?php echo $id ?>"><?php echo $title ?></a></td>
+			    <td data-title="Uploader"><?php echo $name ?></td>
+			    <td data-title="Datum"><?php echo strftime("%e %B %Y", $timestamp) ?></td>
+			    <td data-title="Eigenaar"><?= $owner ?></td>
+			    <td data-title="Acties" class="center"><a class="button" href="single.php?id=<?php echo $id ?>"><i class="fa fa-info"></i>Details</a></td>
+			</tr>
+			<?php
+			    endwhile;
+			?>
+		</tbody>
+	    </table>
+		
+	    <?php
 		}
+		else
+		{
+		    echo "<div class=\"box red\">Er zijn geen resultaten gevonden.</div>";
+		}
+	    }
+		else
+		{
+		    echo "<div class=\"box red\"><ul>";
+
+		foreach ($errors as $error)
+		{
+		    echo "<li>$error</li>";
+		}
+
+		echo "</ul></div>";
+		}
+	    }
 	}
 	?>
-
 </div>
 <?php
 include '../footer.php';
