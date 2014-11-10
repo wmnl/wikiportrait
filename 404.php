590b580f43fd4ae7fd4398b85d062a4aa7ed4135
JurgenNL <JurgenNL@users.noreply.github.com>
2014-11-10 14:54:33 +0100

    fix typo

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
 
 		<h2>Oh nee, deze pagina bestaat niet!</h2>
 
-		<p>Helaas, u bent terecht gekomen op een deel van Wikiportret wat eigenlijk niet bestaat. Het kan ook zijn dat de pagina wel bestaat, maar op dit moment niet beschikbaar is.</p>
+		<p>Helaas, u bent terecht gekomen op een deel van Wikiportret dat eigenlijk niet bestaat. Het kan ook zijn dat de pagina wel bestaat, maar op dit moment niet beschikbaar is.</p>
 		<p>Controleer of u een correcte URL in uw adresbalk heeft staan of keer terug naar de vorige pagina. Pak anders een stroopwafel en probeer het later opnieuw.</p>
 
 </div>
