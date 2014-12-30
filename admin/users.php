2df4f0fc70fea91b1158d3c9328cd38b70b07a9f
Jurgen <jurgen@hotmail.lv>
2014-12-30 15:03:14 +0100

    Users overview uses MeekroDB now.

--- admin/users.php
+++ admin/users.php
@@ -1,58 +1,50 @@
 <?php
-	include '../header.php';
-	include 'tabs.php';
-	checkAdmin();
+    include '../header.php';
+    include 'tabs.php';
+    checkAdmin();
 ?>
 <div id="content">
+    <div class="page-header">
+	<h2>Gebruikersbeheer</h2>
+	<a href="adduser.php" class="button"><i class="fa fa-plus-square fa-lg"></i><span>Nieuwe gebruiker</span></a>
+    </div>
 
-	<div class="page-header">
+    <div class="table-container">
+	<table>
+	    <thead>
+		<tr>
+		    <th class="id center">ID</th>
+		    <th>Gebruikersnaam</th>
+		    <th>OTRS-naam</th>
+		    <th>E-mailadres</th>
+		    <th>Beheerder</th>
+		    <th class="actions-1">Acties</th>
+		</tr>
+	    </thead>
+	    <tbody>
+	    <?php
+		$results = DB::query('SELECT * FROM users');
+		foreach ($results as $row)
+		{
+		    $id = $row['id'];
+		    $username = htmlspecialchars($row['username']);
+		    $otrsname = htmlspecialchars($row['otrsname']);
+		    $email = $row['email'];
+		    $sysop = $row['isSysop'];
 
-		<h2>Gebruikersbeheer</h2>
-
-		<a href="adduser.php" class="button"><i class="fa fa-plus-square fa-lg"></i><span>Nieuwe gebruiker</span></a>
-
-	</div>
-
-	<div class="table-container">
-
-		<table>
-			<thead>
-				<tr>
-				 	<th class="id center">ID</th>
-					<th>Gebruikersnaam</th>
-					<th>OTRS-naam</th>
-					<th>E-mailadres</th>
-					<th>Beheerder</th>
-					<th class="actions-1">Acties</th>
-				</tr>
-			</thead>
-			<tbody>
-			<?php
-				$query = "SELECT * FROM users";
-				$result = mysqli_query($connection, $query);
-				while ($row = mysqli_fetch_assoc($result))
-				{
-					$id 		= $row['id'];
-					$username	= htmlspecialchars($row['username']);
-					$otrsname	= htmlspecialchars($row['otrsname']);
-					$email		= $row['email'];
-					$sysop		= $row['isSysop'];
-
-					echo "<tr>";
-				 	 	echo "<td data-title =\"&#xf02e;\" class=\"center\">$id</td>";
-						echo "<td data-title =\"&#xf007;\">$username</td>";
-						echo "<td data-title =\"&#xf0b1;\">$otrsname</td>";
-						echo "<td data-title =\"&#xf0e0;\">$email</td>";
-						if ($sysop) echo "<td data-title =\"&#xf0f0;\">Ja</td>"; else echo "<td data-title =\"&#xf0f0;\">Nee</td>";
-						echo "<td data-title =\"&#xf0ae;\" class=\"center\"><a class=\"button\" href=\"edituser.php?id=$id\"><i class=\"fa fa-pencil\"></i>Bewerken</a></td>";
-					echo "</tr>";
-				}
-			 ?>
-			 </tbody>
-		</table>
-
-	</div>
-
+		    echo "<tr>";
+			echo "<td data-title =\"&#xf02e;\" class=\"center\">$id</td>";
+			echo "<td data-title =\"&#xf007;\">$username</td>";
+			echo "<td data-title =\"&#xf0b1;\">$otrsname</td>";
+			echo "<td data-title =\"&#xf0e0;\">$email</td>";
+			if ($sysop) echo "<td data-title =\"&#xf0f0;\">Ja</td>"; else echo "<td data-title =\"&#xf0f0;\">Nee</td>";
+			echo "<td data-title =\"&#xf0ae;\" class=\"center\"><a class=\"button\" href=\"edituser.php?id=$id\"><i class=\"fa fa-pencil\"></i>Bewerken</a></td>";
+		    echo "</tr>";
+		}
+	     ?>
+	     </tbody>
+	</table>
+    </div>
 </div>
 <?php
 	include '../footer.php';
