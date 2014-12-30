9ec9ead944dd6555c63ed208f15ad0a96184a1b5
Southparkfan <Southparkfan@users.noreply.github.com>
2014-12-30 17:01:27 +0100

    Remove unused variables.

--- config/config.php
+++ config/config.php
@@ -4,10 +4,7 @@
 	$DBname	  		= ''; // Name of the database.
 	$DBuser	  		= ''; // Name of the databaseuser, must have the appropriate permissions on the database.
 	$DBpassword 		= ''; // Password of the user specified in $DBuser. Leave blank if no password is set (not recommended!).
-	# Mail settings
-	$MailSTMPhost 		= ''; // Name of the STMP host. Most times "smtp.domain.com"
-	$MailRecipient 		= ''; // TODO
-	$MailRecipientName 	= ''; // TODO
+
 	# Developer Settings
 	$DevDebugging 		= false; // Show PHP errors, by default false
 ?>
