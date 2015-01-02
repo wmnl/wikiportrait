5b0ffcd88c6cb5e67efc44f873de9f5f920dc34d
Jurgen <jurgen@hotmail.lv>
2015-01-02 12:22:47 +0100

    Update 404 error file names

--- 404.php
+++ 404.php
@@ -1,16 +1,16 @@
 <?php
-	header('HTTP/1.1 404 Not Found');
-	header('Status: 404 Not Found');
-	include 'header.php';
+    header('HTTP/1.1 404 Not Found');
+    header('Status: 404 Not Found');
+    include 'common/header.php';
 ?>
 <div id="content">
 
-		<h2>Oh nee, deze pagina bestaat niet!</h2>
+    <h2>Oh nee, deze pagina bestaat niet!</h2>
 
-		<p>Helaas, u bent terecht gekomen op een deel van Wikiportret dat eigenlijk niet bestaat. Het kan ook zijn dat de pagina wel bestaat, maar op dit moment niet beschikbaar is.</p>
-		<p>Controleer of u een correcte URL in uw adresbalk heeft staan of keer terug naar de vorige pagina. Pak anders een stroopwafel en probeer het later opnieuw.</p>
+    <p>Helaas, u bent terecht gekomen op een deel van Wikiportret dat eigenlijk niet bestaat. Het kan ook zijn dat de pagina wel bestaat, maar op dit moment niet beschikbaar is.</p>
+    <p>Controleer of u een correcte URL in uw adresbalk heeft staan of keer terug naar de vorige pagina. Pak anders een stroopwafel en probeer het later opnieuw.</p>
 
 </div>
 <?php
-	include 'footer.php';
+    include 'common/footer.php';
 ?>
