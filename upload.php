90f92e296931d3ccbc55dda328c80c104672c6aa
Jurgen <jurgen@hotmail.lv>
2015-03-02 15:24:53 +0100

    use mail() instead of phpmailer

--- upload.php
+++ upload.php
@@ -30,107 +30,90 @@
 
 	    if (move_uploaded_file($file['tmp_name'], "uploads/" . $filename))
 	    {
-		/* Tumpneel maken (Hopelijk doetiet) */
+			/* Tumpneel maken (Hopelijk doetiet) */
 
-		$x=300; //Width
-		$folder="uploads/thumbs"; //Foldername thumbnail
+			$x=300; //Width
+			$folder="uploads/thumbs"; //Foldername thumbnail
 
-		$size = getimagesize("uploads/" . $filename);
+			$size = getimagesize("uploads/" . $filename);
 
-		$scale = $size[0]/$x;
-		$y = $size[1]/$scale;
+			$scale = $size[0]/$x;
+			$y = $size[1]/$scale;
 
-		$thumb = imagecreatetruecolor($x,$y);
+			$thumb = imagecreatetruecolor($x,$y);
 
-		if (in_array($file['type'],array("image/jpeg","image/pjpeg")))
-		{
-			$img=imagecreatefromjpeg("uploads/" . $filename);
-			//$thumb=imagescale($img,$x);
-			imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
-			imagejpeg($thumb,$folder."/".$filename);
+			if (in_array($file['type'],array("image/jpeg","image/pjpeg")))
+			{
+				$img=imagecreatefromjpeg("uploads/" . $filename);
+				//$thumb=imagescale($img,$x);
+				imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
+				imagejpeg($thumb,$folder."/".$filename);
+			}
+			elseif ($file['type']=="image/png")
+			{
+				$img=imagecreatefrompng("uploads/" . $filename);
+				//$thumb=imagescale($img,$x);
+				imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
+				imagepng($thumb,$folder."/".$filename);
+			}
+			elseif ($file['type']=="image/gif")
+			{
+				$img=imagecreatefromgif("uploads/" . $filename);
+				//$thumb=imagescale($img,$x);
+				imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
+				imagegif($thumb,$folder."/".$filename);
+			}
+			elseif ($file['type']=="image/bmp")
+			{
+				$img=imagecreatefromwbmp("uploads/" . $filename);
+				//$thumb=imagescale($img,$x);
+				imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
+				imagewbmp($thumb,$folder."/".$filename);
+			}
+			/* Klaar met Tumpneel */
+
+			DB::insert('images', array (
+				'filename' => $filename,
+				'title' => $title,
+				'source' => $source,
+				'name' => $name,
+				'email' => $email,
+				'license' => 'CC-BY-SA 3.0',
+				'ip' => $ip,
+				'date' => $date,
+				'description' => $desc,
+				'timestamp' => date_timestamp_get($time),
+				'key' => $key
+			));
+
+			$subject = "[Wikiportret] " . $title . " is geüpload op Wikiportret";
+			$body	= "Beste OTRS-vrijwillger, <br />
+	zojuist is er op http://www.wikiportret.nl een nieuwe foto geupload. <br />
+	Deze foto heeft als titel '$title', gemaakt door '$source' onder de licentie 'CC-BY-SA 3.0' met als omschrijving '$desc'<br />
+	De uploader heeft het volgende IP-adres: '$ip'<br />
+	<br />
+	Je kunt de foto bekijken op Wikiportret en de foto daar afwijzen, of een tekst genereren die je kan copy-pasten om een e-mail te schrijven.<br />
+	<br />
+	Klik op deze link:<br />
+	https://www.wikidate.nl/wikiportret/images/single.php?id=" . DB::insertId() . "<br />
+	<br />
+	Als je vragen hebt over de uploadwizard kun je terecht bij JurgenNL via https://commons.wikimedia.org/wiki/User:JurgenNL of eventueel via jurgennl.wp@gmail.com.<br />
+	<br />
+	Al vast heel erg bedankt voor je medewerking!<br />";
+
+			$headers .= "Reply-To: $name <$email>\r\n";
+			$headers .= "Return-Path: $name <$email>\r\n";
+			$headers .= "From: Wikiportret <wikiportret@wikimedia.nl>\r\n"; 
+			$headers .= "MIME-Version: 1.0\r\n";
+			$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+			$headers .= "X-Priority: 3\r\n";
+			$headers .= "X-Mailer: PHP ". phpversion() ."\r\n" ;
+
+			mail("otrs-test@wikimedia.org", $subject, $message, $headers);
+
+			header("Location:wizard.php?question=success&key=" . $key);
+			}
 		}
-		elseif ($file['type']=="image/png")
-		{
-			$img=imagecreatefrompng("uploads/" . $filename);
-			//$thumb=imagescale($img,$x);
-			imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
-			imagepng($thumb,$folder."/".$filename);
-		}
-		elseif ($file['type']=="image/gif")
-		{
-			$img=imagecreatefromgif("uploads/" . $filename);
-			//$thumb=imagescale($img,$x);
-			imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
-			imagegif($thumb,$folder."/".$filename);
-		}
-		elseif ($file['type']=="image/bmp")
-		{
-			$img=imagecreatefromwbmp("uploads/" . $filename);
-			//$thumb=imagescale($img,$x);
-			imagecopyresized($thumb,$img,0,0,0,0,$x,$y,$size[0],$size[1]);
-			imagewbmp($thumb,$folder."/".$filename);
-		}
-		/* Klaar met Tumpneel */
-
-		DB::insert('images', array (
-		    'filename' => $filename,
-		    'title' => $title,
-		    'source' => $source,
-		    'name' => $name,
-		    'email' => $email,
-		    'license' => 'CC-BY-SA 3.0',
-		    'ip' => $ip,
-		    'date' => $date,
-		    'description' => $desc,
-		    'timestamp' => date_timestamp_get($time),
-		    'key' => $key
-		));
-
-		require "PHPMailer/PHPMailerAutoload.php";
-
-		$mail = new PHPMailer;
-
-		$mail->CharSet = "UTF-8";
-		$mail->isSMTP();								  // Set mailer to use SMTP
-
-		$mail->SMTPAuth	= true;					 // enable SMTP authentication
-		$mail->Host	= "mail.wikidate.nl";	 // sets the SMTP server
-		$mail->Port	= 587;					 // set the SMTP port for the GMAIL server
-		$mail->Username	= "info@wikidate.nl";	 // SMTP account username
-		$mail->Password	= "henk";				 // SMTP account password
-
-		$mail->From = 'wikidate@axc.nl';
-		$mail->FromName = 'Wikiportret';
-		$mail->addAddress('jurgen@hotmail.lv', 'otrs-test');			  // Add a recipient
-		$mail->addReplyTo($email, $name);
-
-		$mail->WordWrap = 50;								 // Set word wrap to 50 characters
-		$mail->isHTML(true);								  // Set email format to HTML
-
-		$mail->Subject = "[Wikiportret] " . $title . " is geüpload op Wikiportret";
-		$mail->Body	= "Beste OTRS-vrijwillger, <br />
-zojuist is er op http://www.wikiportret.nl een nieuwe foto geupload. <br />
-Deze foto heeft als titel '$title', gemaakt door '$source' onder de licentie 'CC-BY-SA 3.0' met als omschrijving '$desc'<br />
-De uploader heeft het volgende IP-adres: '$ip'<br />
-<br />
-Je kunt de foto bekijken op Wikiportret en de foto daar afwijzen, of een tekst genereren die je kan copy-pasten om een e-mail te schrijven.<br />
-<br />
-Klik op deze link:<br />
-https://www.wikidate.nl/wikiportret/images/single.php?id=" . DB::insertId() . "<br />
-<br />
-Als je vragen hebt over de uploadwizard kun je terecht bij JurgenNL via https://commons.wikimedia.org/wiki/User:JurgenNL of eventueel via jurgennl.wp@gmail.com.<br />
-<br />
-Al vast heel erg bedankt voor je medewerking!<br />";
-
-		if(!$mail->send()) {
-		   echo "Er is een fout opgetreden tijdens het verzenden van de Wikiportret mail.";
-		   echo "Foutgegevens van de PHPMailer: " . $mail->ErrorInfo;
-		   exit;
-		}
-
-		header("Location:wizard.php?question=success&key=" . $key);
-	    }
-	}
     }
 ?>
 <script>
