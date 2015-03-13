4413a812761e9a9cb0ef467cf6bd95129f4f14f2
Hay Kranen <huskyr@gmail.com>
2015-03-13 17:47:13 +0100

    Added Google Analytics snippet

--- common/header.php
+++ common/header.php
@@ -27,8 +27,20 @@
     <script src="<?= $basispad; ?>/lib/sweetalert/lib/sweet-alert.min.js"></script>
 
 	<title>Wikiportret - Stel uw foto's ter beschikking</title>
-    </head>
-    <body>
+
+    <script>
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-8050986-7']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+</head>
+<body>
 	<div id="all">
 	    <div id="header">
 		<h1><a href="<?= $basispad; ?>/index.php">Wikiportret</a></h1>
