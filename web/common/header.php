50a7120e46bd968c2c436358abbff8922ea0e817
sjoerddebruin <sjoerddebruin@me.com>
2017-02-17 14:15:46 +0100

    Adjust Google Analytics

--- web/common/header.php
+++ web/common/header.php
@@ -21,6 +21,8 @@
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-8050986-7']);
         _gaq.push(['_trackPageview']);
+        _gaq.push(['_gat._forceSSL']);
+        _gaq.push (['_gat._anonymizeIp']);
 
         (function() {
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
