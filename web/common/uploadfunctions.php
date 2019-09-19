bc5703783e4597a2fdb9942f197a4d35598a31ac
Narcode B <f.e.l.i.p.e@icloud.com>
2019-09-19 02:39:24 +0200

    fix mailer class from remko commit 7ca9055

--- web/common/uploadfunctions.php
+++ web/common/uploadfunctions.php
@@ -65,7 +65,7 @@ function checkUpload() {
     $validateUploader = validateUploader();
 
     if (!hasvalidationerrors()) {
-	$mail = new \PHPMailer\PHPMailer\PHPMailer();
+	$mail = new \PHPMailer();
 
 	$templateRenderer = new Handlebars;
   list($email_exists, $email_verified) = contributorEmailCheck($email);
