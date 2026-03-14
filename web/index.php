bce6393edc2fc33893c1e0d9c0b08c7c55c18dbb
Remko de Keijzer <remko@remkodekeijzer.nl>
2026-03-14 15:33:03 +0100

    Free software banner

--- web/index.php
+++ web/index.php
@@ -5,28 +5,45 @@ require_once 'common/header.php';
 ?>
 <div id="content">
     <?php
-    if (!CLOSED) {
-        ?>
-        <h2>Welkom op Wikiportret!</h2>
-        <p>Staat er op Wikipedia een artikel zonder portretfoto? En heeft u een foto die bij een artikel zou passen?
-            Stel dan uw foto hier ter beschikking en de vrijwilligers van Wikipedia doen de rest.</p>
-        <p>Als u veel foto's heeft, of bijvoorbeeld foto's met andere onderwerpen dan beroemdheden overweegt u dan
-            uw foto's onder te brengen bij Wikimedia Commons, de centrale mediadatabank van Wikipedia.</p>
-
-        <div class="bottom center">
-            <a href="wizard.php" class="button green bigbutton"><i class="fa-solid fa-cloud-upload fa-lg"></i>Een
-                portretfoto insturen</a>
-        </div>
+if (!CLOSED) {
+?>
+    <h2>Welkom op Wikiportret!</h2>
+    <p>Staat er op Wikipedia een artikel zonder portretfoto? En heeft u een foto die bij een artikel zou passen?
+        Stel dan uw foto hier ter beschikking en de vrijwilligers van Wikipedia doen de rest.</p>
+    <p>Als u veel foto's heeft, of bijvoorbeeld foto's met andere onderwerpen dan beroemdheden overweegt u dan
+        uw foto's onder te brengen bij Wikimedia Commons, de centrale mediadatabank van Wikipedia.</p>
+    <div class="bottom center">
+        <a href="wizard.php" class="button green bigbutton"><i class="fa-solid fa-cloud-upload fa-lg"></i>Een
+            portretfoto insturen</a>
+    </div>
     <?php } else { ?>
-        <h2>Welkom op Wikiportret!</h2>
-        <p>Helaas is het momenteel niet mogelijk om foto's te uploaden.
-            Het is niet bekend wanneer Wikiportret weer beschikbaar is.
-            U kunt in de tussentijd uw foto rechtstreeks uploaden naar
-            <a href="https://commons.wikimedia.org/wiki/Special:UploadWizard?uselang=nl" target="_blank"
-                rel="noopener nofollow">Wikimedia Commons</a>
-        </p>
-        <p>Onze excuses voor het ongemak<br>Team Wikiportret</p>
+    <h2>Welkom op Wikiportret!</h2>
+    <p>Helaas is het momenteel niet mogelijk om foto's te uploaden.
+        Het is niet bekend wanneer Wikiportret weer beschikbaar is.
+        U kunt in de tussentijd uw foto rechtstreeks uploaden naar
+        <a href="https://commons.wikimedia.org/wiki/Special:UploadWizard?uselang=nl" target="_blank"
+            rel="noopener nofollow">Wikimedia Commons</a>
+    </p>
+    <p>Onze excuses voor het ongemak<br>Team Wikiportret</p>
     <?php } ?>
+
+    <div class="oss-banner" id="ossBanner">
+        <button class="oss-banner__close" id="ossBannerClose" aria-label="Sluiten">&times;</button>
+        <svg class="oss-banner__icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"
+            fill="currentColor" aria-hidden="true">
+            <path d="M12 2a10 10 0 1 0 0 20A10 10 0 0 0 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+        </svg>
+        <span class="oss-banner__text">
+            <strong>Wikiportret is vrije software</strong> &mdash; net als Wikipedia is deze website gebouwd
+            op <a href="https://www.gnu.org/licenses/gpl-2.0.html" target="_blank" rel="noopener noreferrer">open-source
+                software (GPL-2.0)</a> die door vrijwilligers wordt ontwikkeld. Wilt u meehelpen de software te
+            verbeteren? <a href="https://github.com/wmnl/wikiportrait" target="_blank" rel="noopener noreferrer">Bekijk
+                de broncode op GitHub</a> of
+            <a href="https://phabricator.wikimedia.org/tag/wikiportrait/" target="_blank" rel="noopener noreferrer">meld
+                een idee of probleem</a>.
+        </span>
+    </div>
+
 </div>
 <?php
 include 'common/footer.php';
--- web/js/app.js
+++ web/js/app.js
@@ -1,84 +1,95 @@
 /* Autoresize textarea */
-autosize($('textarea'));
+autosize( $( 'textarea' ) );
 
 /* Delete confirmation */
-function confirmDelete(id) {
-    if (confirm("Weet u zeker dat u dit bericht wilt verwijderen?")) {
+function confirmDelete( id ) {
+    if ( confirm( "Weet u zeker dat u dit bericht wilt verwijderen?" ) ) {
         location.href = "deletemessage.php?id=" + id;
     }
 }
 
 /* Add :active to iOS */
-document.addEventListener("touchstart", function () { }, true);
+document.addEventListener( "touchstart", function () { }, true );
 
 /* Do not open links in Safari when in web-app mode */
-if (("standalone" in window.navigator) && window.navigator.standalone) {
+if ( ( "standalone" in window.navigator ) && window.navigator.standalone ) {
     var noddy, remotes = false;
 
-    document.addEventListener('click', function (event) {
+    document.addEventListener( 'click', function ( event ) {
 
         noddy = event.target;
 
-        while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+        while ( noddy.nodeName !== "A" && noddy.nodeName !== "HTML" ) {
             noddy = noddy.parentNode;
         }
 
-        if ('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
+        if ( 'href' in noddy && noddy.href.indexOf( 'http' ) !== -1 && ( noddy.href.indexOf( document.location.host ) !== -1 || remotes ) ) {
             event.preventDefault();
             document.location.href = noddy.href;
         }
 
-    }, false);
+    }, false );
 }
 
 /* Prevent more than 1 click on upload button */
-function disableButton(e) {
-    f = document.querySelector('form');
-    if (!f.checkValidity()) {
+function disableButton( e ) {
+    f = document.querySelector( 'form' );
+    if ( !f.checkValidity() ) {
         f.reportValidity();
     } else {
         e.disabled = true;
         p = e.parentElement;
         e.style.display = 'none';
-        img = document.createElement('img');
+        img = document.createElement( 'img' );
         img.style.display = 'inline';
         img.style.height = '42px';
         img.src = 'js/loading.gif';
         img.style.zindex = 2;
-        p.appendChild(img);
-        setTimeout(function () {
+        p.appendChild( img );
+        setTimeout( function () {
             f.submit();
-        }, 100);
+        }, 100 );
     }
 }
-$('body').on('change', '#bot', function () {
-    if ($('#bot').is(":checked")) {
-        $('#key').parent('.input-container').removeClass('d-none');
-        $('#key').val(generateRandomPasswordSelection(16));
+$( 'body' ).on( 'change', '#bot', function () {
+    if ( $( '#bot' ).is( ":checked" ) ) {
+        $( '#key' ).parent( '.input-container' ).removeClass( 'd-none' );
+        $( '#key' ).val( generateRandomPasswordSelection( 16 ) );
     } else {
-        $('#key').parent('.input-container').addClass('d-none');
+        $( '#key' ).parent( '.input-container' ).addClass( 'd-none' );
     }
-});
-const generateRandomPasswordSelection = (length) => {
-    const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const special = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', ':', ';', '?', ', ', '.', '|', '\\'];
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+} );
+const generateRandomPasswordSelection = ( length ) => {
+    const uppercase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+    const lowercase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+    const special = [ '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', ':', ';', '?', ', ', '.', '|', '\\' ];
+    const numbers = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
 
-    const nonSpecial = [...uppercase, ...lowercase, ...numbers];
+    const nonSpecial = [ ...uppercase, ...lowercase, ...numbers ];
 
     let password = '';
 
-    for (let i = 0; i < length; i++) {
+    for ( let i = 0; i < length; i++ ) {
         // Previous character is a special character
-        if (i !== 0 && special.includes(password[i - 1])) {
-            password += getRandomElement(nonSpecial);
-        } else password += getRandomElement([...nonSpecial, ...special]);
+        if ( i !== 0 && special.includes( password[ i - 1 ] ) ) {
+            password += getRandomElement( nonSpecial );
+        } else password += getRandomElement( [ ...nonSpecial, ...special ] );
     }
 
     return password;
-}
+};
 const getRandomElement = arr => {
-    const rand = Math.floor(Math.random() * arr.length);
-    return arr[rand];
+    const rand = Math.floor( Math.random() * arr.length );
+    return arr[ rand ];
+};
+// ===== Open-source banner: eenmalig sluiten =====
+if ( localStorage.getItem( 'ossBannerDismissed' ) === '1' ) {
+    $( '#ossBanner' ).hide();
 }
+
+$( document ).on( 'click', '#ossBannerClose', function () {
+    $( '#ossBanner' ).fadeOut( 200 );
+    try {
+        localStorage.setItem( 'ossBannerDismissed', '1' );
+    } catch ( e ) { /* private-browsing: negeer */ }
+} );
